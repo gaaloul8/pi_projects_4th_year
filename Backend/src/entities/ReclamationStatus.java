f341bf3fe0732d020eff32559771027132a4d599
maryem-dridi <124693760+maryem-dridi@users.noreply.github.com>
2024-04-03 18:36:56 +0100

    Revert "Mariem dridi"

--- Backend/src/entities/ReclamationStatus.java
+++
@@ -1,7 +0,0 @@
-package com.esprit.pi_project.entities;
-
-public enum ReclamationStatus {
-    SUBMITTED,
-    IN_PROGRESS,
-    RESOLVED
-}
--- Backend/src/main/java/com/esprit/pi_project/entities/Reclamation.java
+++ Backend/src/main/java/com/esprit/pi_project/entities/Reclamation.java
@@ -1,45 +1,43 @@
 package com.esprit.pi_project.entities;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
-
+@Data
 @Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Reclamation implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
     private int reclamationId;
-
-    @Column(nullable = false, length = 100)
+    @Column(
+            nullable = false,
+            length = 100
+    )
     private String title;
-
-    @Column(nullable = false, length = 500)
+    @Column(
+            nullable = false,
+            length = 500
+    )
     private String description;
-
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(
+            nullable = false
+    )
     private ReclamationStatus status;
 
     @ManyToOne
-    @JoinColumn(name = "created_by_user_id", nullable = false)
-    @JsonBackReference
+    @JoinColumn(name = "id_user")
     private User createdBy;
 
-    @Column(nullable = false)
+    @Column(
+            nullable = false
+    )
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdAt = new Date();
-
-    @Column(name = "is_archived", nullable = false)
-    private boolean isArchived = false;
 
 }
