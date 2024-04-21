965cf5280298ea2f3b40463da95ce8ec039b5032
maryem-dridi <124693760+maryem-dridi@users.noreply.github.com>
2024-04-21 15:11:42 +0100

    Delete Backend/src/repositories directory

--- Backend/src/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.esprit.pi_project.repositories;
-
-import com.esprit.pi_project.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
