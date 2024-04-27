065ce1109a65208421a03d202a22408500d02dc8
SadokSassi1 <sadok.sassi@esprit.tn>
2024-04-27 23:28:04 +0100

    stats for post and club

--- Backend/src/main/java/com/esprit/pi_project/entities/Post.java
+++ Backend/src/main/java/com/esprit/pi_project/entities/Post.java
@@ -2,19 +2,28 @@ package com.esprit.pi_project.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.sql.Blob;
 import java.util.Date;
 import java.util.List;
 @Entity
 @Data
 @Table(name = "post")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Post implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
+    @Lob
+    @Column(nullable = true, length = 10000000)
     private String image;
     private Date postDate;
     private String content;
